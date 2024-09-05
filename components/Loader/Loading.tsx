779d8dd1ad0159d6f6a131fc609f9f116958d280
rishijain2002 <rishijain0652@gmail.com>
2024-09-05 18:57:34 +0530

    Remove prop

--- components/Loader/Loading.tsx
+++ components/Loader/Loading.tsx
@@ -2,9 +2,7 @@
 import React from 'react';
 import styles from './style.module.css'; // Import the CSS module
 
-type Props = {}
-
-const Loading: React.FC<Props> = () => {
+const Loading: React.FC = () => {
   return (
     <div className={styles['dot-spinner']}>
         <div className={styles['dot-spinner__dot']}></div>
